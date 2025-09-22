4a8eededaa3a7ba329c0a3499c8d15292287d8b1
meladmaher <kharboshe4@gmail.com>
2025-09-22 23:59:08 +0200

    Edit file

--- js/script.js
+++ js/script.js
@@ -1897,7 +1897,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then(() => console.log("Service Worker Registered"))
       .catch((err) => console.log("SW registration failed: ", err));
   });
